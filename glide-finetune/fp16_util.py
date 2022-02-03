Fix respacing bugs. Fix exploding gradients (mostly). Add sgd option.

glide-finetune/fp16_util.py
@@ -7,7 +7,7 @@ import torch as th
 import torch.nn as nn
 from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 
-INITIAL_LOG_LOSS_SCALE = 14.0
+INITIAL_LOG_LOSS_SCALE = 20.0 # Default from OpenAI. May wish to change this for finetuning.
 
 
 def convert_module_to_f16(l):

glide-finetune/glide-finetune.py
@@ -1,3 +1,4 @@
+import time
 from torch.cuda.amp import autocast
 from torch.nn.utils import clip_grad_norm_
 import bitsandbytes as bnb
@@ -14,35 +15,36 @@ import fp16_util
 import util
 from loader import TextImageDataset
 
+
 def train_step(
     glide_model: Text2ImUNet,
     glide_diffusion: SpacedDiffusion,
-    glide_options: dict,
     batch: Tuple[th.Tensor, th.Tensor, th.Tensor],
     device: str,
-    respace: bool = True,
 ):
-    with th.no_grad():
-        tokens, masks, x_start = batch
-        tokens, masks, x_start = tokens.to('cpu'), masks.to('cpu'), x_start.to('cpu')
-        x_start = ( x_start.permute(0, 3, 1, 2).float() / 127.5 - 1).cpu()  # normalize to [-1, 1], shape: [batch_size, channels, side_x, side_y]
-        timesteps = th.randint(1, len(glide_diffusion.betas) - 1, (x_start.shape[0],), device='cpu')  # random timesteps
-        noise = th.randn_like(x_start, device='cpu')  # random noise
-        if respace:
-            timesteps = timesteps // ( glide_options["diffusion_steps"] // len(glide_diffusion.betas))
-        
-        x_t = glide_diffusion.q_sample(x_start, timesteps, noise=noise)  # sample from diffusion
-        B, C = x_t.shape[:2]
-
-    tokens = tokens.to(device)
-    masks = masks.to(device)
-    timesteps = timesteps.to(device)
-    x_t = x_t.to(device)
-    noise = noise.to(device)
-    model_output = glide_model(x_t, timesteps, tokens=tokens, mask=masks,) 
-    assert model_output.shape == (B, C * 2, *x_t.shape[2:])
+    tokens, masks, x_start = [
+        x.to(device) for x in batch
+    ]  # Put the batch on the device
+    timesteps = th.randint(
+        0, len(glide_diffusion.betas) - 1, (x_start.shape[0],), device=device
+    )  # random timesteps
+    noise = th.randn_like(x_start, device=device)  # random noise
+    x_t = glide_diffusion.q_sample(x_start, timesteps, noise=noise).to(
+        device
+    )  # What the image should look like for the given timestep/noise
+    B, C = x_t.shape[:2]
+    model_output = glide_model(
+        x_t.to(device),
+        timesteps.to(device),
+        tokens=tokens.to(device),
+        mask=masks.to(device),
+    ).to(
+        device
+    )  # forward pass
     epsilon, _ = th.split(model_output, C, dim=1)
-    return util.mean_flat((noise.detach() - epsilon) ** 2)  # compute loss
+    return util.mean_flat(
+        (noise.detach().to(device) - epsilon) ** 2
+    ).mean()  # compute loss
 
 
 def run_glide_finetune_epoch(
@@ -51,61 +53,77 @@ def run_glide_finetune_epoch(
     glide_options: dict,
     dataloader: th.utils.data.DataLoader,
     optimizer: th.optim.Optimizer,
-    batch_size: int,
-    prompt: str = "",
+    sample_bs: int,  # batch size for inference, not training
+    sample_gs: float = 4.0,  # guidance scale for inference, not training
+    prompt: str = "",  # prompt for inference, not training
     side_x: int = 64,
     side_y: int = 64,
     outputs_dir: str = "./outputs",
     checkpoints_dir: str = "./finetune_checkpoints",
     device: str = "cpu",
     log_frequency: int = 100,
-    wandb_run = None,
+    wandb_run=None,
 ):
     os.makedirs(checkpoints_dir, exist_ok=True)
-    trainer = fp16_util.MixedPrecisionTrainer(model=glide_model, use_fp16=glide_options["use_fp16"], fp16_scale_growth=0.001)
+    trainer = fp16_util.MixedPrecisionTrainer(
+        model=glide_model, use_fp16=glide_options["use_fp16"], fp16_scale_growth=0.001
+    )
     trainer.zero_grad()
     for train_idx, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
         current_loss = train_step(
             glide_model=glide_model,
             glide_diffusion=glide_diffusion,
-            glide_options=glide_options,
             batch=batch,
             device=device,
         )
-        current_loss = current_loss.mean()
         trainer.backward(current_loss)
         trainer.optimize(opt=optimizer)
         trainer.zero_grad()  # clear the gradients
 
-        log = {} # 
+        log = {}  #
         if train_idx % 10:
             tqdm.write(f"loss: {current_loss.item():.4f}")
-            log = { **log, 'iter': train_idx, 'loss': current_loss.item() }
+            log = {**log, "iter": train_idx, "loss": current_loss.item()}
 
         # Sample from the model
-        if train_idx % log_frequency == 0:
-            with th.no_grad():
-                print(f"Sampling from model at iteration {train_idx}")
-                samples = util.sample(
-                    model=glide_model, eval_diffusion=glide_diffusion,
-                    options=glide_options, side_x=side_x,
-                    side_y=side_y, prompt=prompt,
-                    batch_size=batch_size, guidance_scale=1,
-                    device=device,
-                )
-                sample_save_path = os.path.join(outputs_dir, f"{train_idx}.png")
-                util.pred_to_pil(samples).save(sample_save_path)
-                log = { **log, 'iter': train_idx, 'samples': wandb.Image(sample_save_path, caption=prompt) }
-                print(f"Saved sample {sample_save_path}")
+        if train_idx > 0 and train_idx % log_frequency == 0:
+            print(f"Sampling from model at iteration {train_idx}")
+            samples = util.sample(
+                glide_model=glide_model,
+                glide_options=glide_options,
+                side_x=side_x,
+                side_y=side_y,
+                prompt=prompt,
+                batch_size=sample_bs,
+                guidance_scale=sample_gs,
+                device=device,
+                prediction_respacing='27' # TODO use args
+            )
+            sample_save_path = os.path.join(outputs_dir, f"{train_idx}.png")
+            util.pred_to_pil(samples).save(sample_save_path)
+            log = {
+                **log,
+                "iter": train_idx,
+                "samples": wandb.Image(sample_save_path, caption=prompt),
+            }
+            print(f"Saved sample {sample_save_path}")
         if train_idx % 1000 == 0 and train_idx > 0:
             save_model(glide_model, checkpoints_dir, train_idx)
+            print(
+                f"Saved checkpoint {train_idx} to {checkpoints_dir}/glide-ft-{train_idx}.pt"
+            )
         wandb_run.log(log)
     print(f"Finished training, saving final checkpoint")
     save_model(glide_model, checkpoints_dir, train_idx)
 
+
 def save_model(glide_model: Text2ImUNet, checkpoints_dir: str, train_idx: int):
-    th.save(glide_model.state_dict(), os.path.join(checkpoints_dir, f"glide-ft-{train_idx}.pt"))
+    th.save(
+        glide_model.state_dict(),
+        os.path.join(checkpoints_dir, f"glide-ft-{train_idx}.pt"),
+    )
     print(f"Saved checkpoint {train_idx} to {checkpoints_dir}/glide-ft-{train_idx}.pt")
+
 
 def run_glide_finetune(
     data_dir="./data",
@@ -115,7 +133,6 @@ def run_glide_finetune(
     side_x=64,
     side_y=64,
     resize_ratio=1.0,
-    timestep_respacing="1000",
     uncond_p=0.0,
     resume_ckpt="",
     checkpoints_dir="./finetune_checkpoints",
@@ -129,6 +146,9 @@ def run_glide_finetune(
     num_epochs=100,
     log_frequency=100,
     test_prompt="a group of skiers are preparing to ski down a mountain.",
+    sample_bs=1,
+    sample_gs=4.0,
+    use_sgd=False,
 ):
     if "~" in data_dir:
         data_dir = os.path.expanduser(data_dir)
@@ -157,15 +177,10 @@ def run_glide_finetune(
         glide_path=resume_ckpt,
         use_fp16=use_fp16,
         dropout=dropout,
-        timestep_respacing=timestep_respacing,
         freeze_transformer=freeze_transformer,
         freeze_diffusion=freeze_diffusion,
         activation_checkpointing=activation_checkpointing,
     )
-    glide_model.zero_grad()
-    glide_model.train()
-    glide_model.to(device)
-    glide_model.requires_grad_(True)
 
     # Data setup
     dataset = TextImageDataset(
@@ -192,16 +207,26 @@ def run_glide_finetune(
     )
 
     # Optimizer setup
-    optimizer = th.optim.SGD(
-        [x for x in glide_model.parameters() if x.requires_grad],
-        lr=learning_rate,
-    )
-
+    if use_sgd:
+        optimizer = th.optim.SGD(
+            [x for x in glide_model.parameters() if x.requires_grad],
+            lr=learning_rate,
+            momentum=0.9,
+        )
+    else:
+        optimizer = th.optim.AdamW(
+            [x for x in glide_model.parameters() if x.requires_grad],
+            lr=learning_rate,
+            weight_decay=0.0,
+        )
 
     # Training setup
     outputs_dir = "./outputs"
     os.makedirs(outputs_dir, exist_ok=True)
 
+    glide_model.to(device)
+    glide_model.train()
+    glide_model.zero_grad()
     for epoch in range(num_epochs):
         print(f"Starting epoch {epoch}")
         run_glide_finetune_epoch(
@@ -211,9 +236,10 @@ def run_glide_finetune(
             optimizer=optimizer,
             dataloader=dataloader,
             prompt=test_prompt,
+            sample_bs=sample_bs,
+            sample_gs=sample_gs,
             checkpoints_dir=checkpoints_dir,
             outputs_dir=outputs_dir,
-            batch_size=batch_size,
             side_x=side_x,
             side_y=side_y,
             device=device,
@@ -228,13 +254,14 @@ def parse_args():
     parser.add_argument("--batch_size", "-bs", type=int, default=1)
     parser.add_argument("--learning_rate", "-lr", type=float, default=2e-5)
     parser.add_argument("--dropout", "-drop", type=float, default=0.1)
-    parser.add_argument("--timestep_respacing", "-respace", type=str, default="1000")
     parser.add_argument("--side_x", "-x", type=int, default=64)
     parser.add_argument("--side_y", "-y", type=int, default=64)
     parser.add_argument("--resize_ratio", "-crop", type=float, default=0.8)
     parser.add_argument("--uncond_p", "-p", type=float, default=0.0)
     parser.add_argument("--resume_ckpt", "-resume", type=str, default="")
-    parser.add_argument("--checkpoints_dir", "-ckpt", type=str, default="./glide_checkpoints/")
+    parser.add_argument(
+        "--checkpoints_dir", "-ckpt", type=str, default="./glide_checkpoints/"
+    )
     parser.add_argument("--use_fp16", "-fp16", action="store_true")
     parser.add_argument("--device", "-dev", type=str, default="")
     parser.add_argument("--log_frequency", "-freq", type=int, default=100)
@@ -244,9 +271,17 @@ def parse_args():
     parser.add_argument("--activation_checkpointing", "-grad_ckpt", action="store_true")
     parser.add_argument("--use_captions", "-txt", action="store_true")
     parser.add_argument("--epochs", "-epochs", type=int, default=20)
-    parser.add_argument("--test_prompt", "-prompt", type=str, default="a group of skiers are preparing to ski down a mountain.")
+    parser.add_argument(
+        "--test_prompt",
+        "-prompt",
+        type=str,
+        default="a group of skiers are preparing to ski down a mountain.",
+    )
+    parser.add_argument("--test_batch_size", "-tbs", type=int, default=1, help="Batch size used for model eval, not training.")
+    parser.add_argument("--test_guidance_scale", "-tgs", type=float, default=1.0, help="Guidance scale used during model eval, not training.")
+    parser.add_argument("--use_sgd", "-sgd", action="store_true")
     args = parser.parse_args()
-    return args 
+    return args
 
 
 if __name__ == "__main__":
@@ -257,11 +292,16 @@ if __name__ == "__main__":
     else:
         device = th.device("cpu") if not th.cuda.is_available() else th.device("cuda")
 
+    # Pretty print args
+
+    for arg in vars(args):
+        print(f"{arg}: {getattr(args, arg)}")
+    time.sleep(5)
+
     run_glide_finetune(
         data_dir=args.data_dir,
         batch_size=args.batch_size,
         learning_rate=args.learning_rate,
-        timestep_respacing=args.timestep_respacing,
         dropout=args.dropout,
         side_x=args.side_x,
         side_y=args.side_y,
@@ -279,4 +319,7 @@ if __name__ == "__main__":
         use_captions=args.use_captions,
         num_epochs=args.epochs,
         test_prompt=args.test_prompt,
+        use_sgd=args.use_sgd,
+        sample_bs=args.test_batch_size,
+        sample_gs=args.test_guidance_scale,
     )

glide-finetune/loader.py
@@ -20,6 +20,7 @@ def get_uncond_tokens_mask(tokenizer: Encoder):
     return th.tensor(uncond_tokens), th.tensor(uncond_mask, dtype=th.bool)
 
 
+@lru_cache(maxsize=16000)
 def get_tokens_and_mask(
     tokenizer: Encoder, prompt: str = ""
 ) -> Tuple[th.tensor, th.tensor]:
@@ -103,6 +104,7 @@ class TextImageDataset(Dataset):
                     ratio=(1.0, 1.0),
                     interpolation=T.InterpolationMode.LANCZOS,
                 ),
+                T.ToTensor()
             ]
         )
 
@@ -148,7 +150,6 @@ class TextImageDataset(Dataset):
             tokens, mask = self.get_caption(ind)
         try:
             x_img = self.imagepreproc(PIL.Image.open(image_file))
-            x_img = np.asarray(self.imagepreproc(x_img))
         except (OSError, ValueError) as e:
             print(f"An exception occurred trying to load file {image_file}.")
             print(f"Skipping index {ind}")

glide-finetune/util.py
@@ -1,13 +1,14 @@
-import numpy as np
-from PIL import Image
-import wandb
-import torch as th
-from glide_text2im.download import load_checkpoint
 import os
+
+import torch as th
+import wandb
+from glide_text2im.download import load_checkpoint
 from glide_text2im.model_creation import (
+    create_gaussian_diffusion,
     create_model_and_diffusion,
     model_and_diffusion_defaults,
 )
+from PIL import Image
 
 
 def extract_into_tensor(arr, timesteps, broadcast_shape):
@@ -16,10 +17,12 @@ def extract_into_tensor(arr, timesteps, broadcast_shape):
         res = res[..., None]
     return res.expand(broadcast_shape)
 
+
 def pred_to_pil(pred: th.Tensor) -> Image:
     scaled = ((pred + 1) * 127.5).round().clamp(0, 255).to(th.uint8).cpu()
     reshaped = scaled.permute(2, 0, 3, 1).reshape([pred.shape[2], -1, 3])
     return Image.fromarray(reshaped.numpy())
+
 
 def mean_flat(tensor):
     """
@@ -29,59 +32,76 @@ def mean_flat(tensor):
 
 
 def load_base_model(
-    glide_path:str='',
-    use_fp16:bool=False,
-    dropout: float=0.1,
-    timestep_respacing: str='1000',
+    glide_path: str = "",
+    use_fp16: bool = False,
+    dropout: float = 0.1,
     freeze_transformer: bool = False,
     freeze_diffusion: bool = False,
-    activation_checkpointing: bool = False,):
+    activation_checkpointing: bool = False,
+):
 
     options = model_and_diffusion_defaults()
-    options['use_fp16'] = use_fp16
-    options['dropout'] = dropout
-    options['timestep_respacing'] = timestep_respacing
+    options["use_fp16"] = use_fp16
+    options["dropout"] = dropout
     glide_model, glide_diffusion = create_model_and_diffusion(**options)
-    if use_fp16:
-        glide_model.convert_to_fp16()
+    if activation_checkpointing:
+        glide_model.use_checkpoint = True
     if freeze_transformer:
-        glide_model.requires_grad_(True)
-        glide_model.transformer.requires_grad_(False) # freeze transformer
+        glide_model.requires_grad_(False)
+        glide_model.time_embed.requires_grad_(True)
+        glide_model.input_blocks.requires_grad_(True)
+        glide_model.middle_blocks.requires_grad_(True)
+        glide_model.output_blocks.requires_grad_(True)
     elif freeze_diffusion:
-        glide_model.requires_grad_(False) # freeze everything,
-        glide_model.transformer.requires_grad_(True) # then unfreeze transformer
+        glide_model.requires_grad_(False)
+        glide_model.transformer.requires_grad_(True)  # then unfreeze transformer
         glide_model.transformer_proj.requires_grad_(True)
         glide_model.token_embedding.requires_grad_(True)
         glide_model.positional_embedding.requires_grad_(True)
         glide_model.padding_embedding.requires_grad_(True)
         glide_model.final_ln.requires_grad_(True)
+        glide_model.time_embed.requires_grad_(True)
     else:
-        glide_model.requires_grad_(True) # unfreeze everything
-    if activation_checkpointing:
-        glide_model.use_checkpoint = True
-    if len(glide_path) > 0: # user provided checkpoint
-        assert os.path.exists(glide_path), 'glide path does not exist'
-        weights = th.load(glide_path, map_location='cpu')
+        glide_model.requires_grad_(True)  # unfreeze everything
+    if len(glide_path) > 0:  # user provided checkpoint
+        assert os.path.exists(glide_path), "glide path does not exist"
+        weights = th.load(glide_path, map_location="cpu")
         glide_model.load_state_dict(weights)
-    else: # use default checkpoint from openai
-        glide_model.load_state_dict(load_checkpoint('base', 'cpu'))
+    else:  # use default checkpoint from openai
+        glide_model.load_state_dict(
+            load_checkpoint("base", "cpu")
+        )  # always load to cpu, saves memory
     return glide_model, glide_diffusion, options
 
 
 # Sample from the base model.
-def sample(model, eval_diffusion, options, side_x, side_y, prompt='', batch_size=1, guidance_scale=4, device='cpu'):
-    model.del_cache()
-
+@th.no_grad()
+def sample(
+    glide_model,
+    glide_options,
+    side_x,
+    side_y,
+    prompt="",
+    batch_size=1,
+    guidance_scale=4,
+    device="cpu",
+    prediction_respacing="100",
+):
+    eval_diffusion = create_gaussian_diffusion(
+        steps=glide_options["diffusion_steps"],
+        noise_schedule=glide_options["noise_schedule"],
+        timestep_respacing=prediction_respacing,
+    )
     # Create the text tokens to feed to the model.
-    tokens = model.tokenizer.encode(prompt)
-    tokens, mask = model.tokenizer.padded_tokens_and_mask(
-        tokens, options['text_ctx']
+    tokens = glide_model.tokenizer.encode(prompt)
+    tokens, mask = glide_model.tokenizer.padded_tokens_and_mask(
+        tokens, glide_options["text_ctx"]
     )
 
     # Create the classifier-free guidance tokens (empty)
     full_batch_size = batch_size * 2
-    uncond_tokens, uncond_mask = model.tokenizer.padded_tokens_and_mask(
-        [], options['text_ctx']
+    uncond_tokens, uncond_mask = glide_model.tokenizer.padded_tokens_and_mask(
+        [], glide_options["text_ctx"]
     )
 
     # Pack the tokens together into model kwargs.
@@ -96,20 +116,29 @@ def sample(model, eval_diffusion, options, side_x, side_y, prompt='', batch_size
         ),
     )
     with th.inference_mode():
+
         def model_fn(x_t, ts, **kwargs):
             half = x_t[: len(x_t) // 2]
             combined = th.cat([half, half], dim=0)
-            model_out = model(combined, ts, **kwargs)
+            model_out = glide_model(combined, ts, **kwargs)
             eps, rest = model_out[:, :3], model_out[:, 3:]
             cond_eps, uncond_eps = th.split(eps, len(eps) // 2, dim=0)
-            beta = eval_diffusion.betas[int(ts.flatten()[0].item() / options["diffusion_steps"] * len(eval_diffusion.betas))]
+            beta = eval_diffusion.betas[
+                int(
+                    ts.flatten()[0].item()
+                    / glide_options["diffusion_steps"]
+                    * len(eval_diffusion.betas)
+                )
+            ]
             half_eps = uncond_eps + guidance_scale * (cond_eps - uncond_eps)
             eps = th.cat([half_eps, half_eps], dim=0)
-            current_prediction_pil = pred_to_pil((x_t - eps * (beta ** 0.5))[:batch_size])
-            current_prediction_pil.save('current_prediction.png')
+            current_prediction_pil = pred_to_pil(
+                (x_t - eps * (beta ** 0.5))[:batch_size]
+            )
+            current_prediction_pil.save("current_prediction.png")
             return th.cat([eps, rest], dim=1)
 
-        samples = eval_diffusion.ddim_sample_loop(
+        samples = eval_diffusion.p_sample_loop(
             model_fn,
             (full_batch_size, 3, side_y, side_x),  # only thing that's changed
             device=device,
@@ -118,9 +147,7 @@ def sample(model, eval_diffusion, options, side_x, side_y, prompt='', batch_size
             model_kwargs=model_kwargs,
             cond_fn=None,
         )[:batch_size]
-    model.del_cache()
     return samples
-
 
 
 def wandb_setup(
@@ -132,7 +159,7 @@ def wandb_setup(
     device: str,
     data_dir: str,
     base_dir: str,
-    project_name: str = 'glide-text2im-finetune',
+    project_name: str = "glide-text2im-finetune",
 ):
     return wandb.init(
         project=project_name,
@@ -147,47 +174,3 @@ def wandb_setup(
             "base_dir": base_dir,
         },
     )
-
-
-"""Exponential moving average for PyTorch. Adapted from
-https://www.zijianhu.com/post/pytorch/ema/.
-"""
-
-from copy import deepcopy
-
-from torch import nn
-
-
-class EMA(nn.Module):
-    def __init__(self, model, decay):
-        super().__init__()
-        self.model = model
-        self.decay = decay
-        self.average = deepcopy(self.model)
-        for param in self.average.parameters():
-            param.detach_()
-
-    @th.no_grad()
-    def update(self):
-        if not self.training:
-            raise RuntimeError('Update should only be called during training')
-
-        model_params = dict(self.model.named_parameters())
-        average_params = dict(self.average.named_parameters())
-        assert model_params.keys() == average_params.keys()
-
-        for name, param in model_params.items():
-            average_params[name].mul_(self.decay)
-            average_params[name].add_((1 - self.decay) * param)
-
-        model_buffers = dict(self.model.named_buffers())
-        average_buffers = dict(self.average.named_buffers())
-        assert model_buffers.keys() == average_buffers.keys()
-
-        for name, buffer in model_buffers.items():
-            average_buffers[name].copy_(buffer)
-
-    def forward(self, *args, **kwargs):
-        if self.training:
-            return self.model(*args, **kwargs)
-        return self.average(*args, **kwargs)