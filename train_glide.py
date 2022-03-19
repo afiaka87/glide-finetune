f6d5c88cd6b3e9e1f8c36aa7b3229b1ed4ebc272
tillfalko <tillfalko@gmail.com>
2022-03-19 19:17:13 +0100

    --freeze_diffusion

--- train_glide.py
+++ train_glide.py
@@ -135,6 +135,11 @@ def run_glide_finetune(
         lr=learning_rate,
         weight_decay=adam_weight_decay,
     )
+    if not freeze_transformer: # if we want to train the transformer, we need to backpropagate through the diffusion model.
+        glide_model.out.requires_grad_(True)
+        glide_model.input_blocks.requires_grad_(True)
+        glide_model.middle_block.requires_grad_(True)
+        glide_model.output_blocks.requires_grad_(True)
 
     # Training setup
     outputs_dir = "./outputs"
